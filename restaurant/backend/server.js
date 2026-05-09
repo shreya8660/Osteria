90d536f6b9b7695f1d0ed57ae6d4e1a7d5c07006
shreya8660 <shreyavenkatesh2005@gmail.com>
2026-05-09 13:06:26 +0530

    Updated production CORS

--- restaurant/backend/server.js
+++ restaurant/backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: "https://osteria-3.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 // DB Connection
